844ea4d894d8dc2e97216fcc90be1eb45e5b72f6
AaronTao16 <aarontao16@outlook.com>
2021-11-26 10:49:29 -0500

    app

---
+++ src/main/java/edu/pitt/coursesearch/CourseSearchApplication.java
@@ -0,0 +1,13 @@
+package edu.pitt.coursesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/pitt/coursesearch/CourseSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pitt.coursesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
