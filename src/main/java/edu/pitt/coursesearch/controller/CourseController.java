5f3ae8f1ce1fdab66307ac021609a45621e608b4
AaronTao16 <aarontao16@outlook.com>
2021-11-30 11:13:34 -0500

    controller

--- src/main/java/edu/pitt/coursesearch/controller/CourseController.java
+++ src/main/java/edu/pitt/coursesearch/controller/CourseController.java
@@ -1,15 +1,17 @@
 package edu.pitt.coursesearch.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/coursedata")
 public class CourseController {
 
     @ResponseBody
-    @RequestMapping("/")
+    @GetMapping("/")
     public String getCourses(Model model){
 //        model.addAllAttributes("list", "123");
         return "Portal";
